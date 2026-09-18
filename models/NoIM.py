4d84d6116ccac8a119d71201f85455e490ff9e0d
nextWorld <147847553+Idwenx@users.noreply.github.com>
2026-09-18 15:10:13 +0800

    clear

--- models/NoIM.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-class NoIM(object):
-    def select_action(self, graph_rep, nf, mask):
-        return torch.LongTensor([])
-    
-    def load(self, filename, lr=None):
-        pass
